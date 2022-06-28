77dd207dfcc9399e37d956f57410c9d913d5474b
Arjumand-coder <98021606+Arjumand-coder@users.noreply.github.com>
2022-06-28 22:17:04 +0930

    Add files via upload

---
+++ kwitter-room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyACc9XbOTmPBdjIIkAS9JDBY1bWQC26aQY",
+    authDomain: "kwitter-7a709.firebaseapp.com",
+    databaseURL: "https://kwitter-7a709-default-rtdb.firebaseio.com",
+    projectId: "kwitter-7a709",
+    storageBucket: "kwitter-7a709.appspot.com",
+    messagingSenderId: "723885758892",
+    appId: "1:723885758892:web:2e47af8b82e23cf1714209"
+  };
+  
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
